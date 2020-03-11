9509573986bb6a9758137ab14ab8c9303fb45206
thecassion <pierrerobentz.cassion@gmail.com>
2020-03-11 14:29:25 -0400

    Change the package name

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import {Table} from 'react-materialui-table'
+import {Table} from '@thecassion/react-material-ui-table'
 
 export default class App extends Component {
   render () {
