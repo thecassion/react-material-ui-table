940ec2f22b6007743fc8c77a739bb63fdf3ebe06
thecassion <pierrerobentz.cassion@gmail.com>
2020-04-05 19:11:41 -0400

    Add filter

---
+++ src/Export/export.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactExport from "react-data-export";
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import PropTypes from 'prop-types';
+
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+
+const Exporter=(props)=>{
+    return (
+      <ExcelFile element={<Tooltip title={props.title}>
+      <IconButton aria-label="Export list"
+      classeName={{ root: classes.icon }}>
+        <CloudDownloadIcon />
+      </IconButton>
+    </Tooltip>}>
+      <ExcelSheet data={props.data} name={props.sheetName}>
+        {props.columns.map(
+          (column, index)=>(<ExcelColumn label={column.label} value={column.id}/>)
+        )}
+      </ExcelSheet>
+  </ExcelFile>
+    )
+  }
+
+  Exporter.propTypes = {
+    data: PropTypes.array,
+    title: PropTypes.string,
+    sheetName: PropTypes.name
+  };
+
+  export default Exporter;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export {default as Table} from './Table/Table';
+export {default as Export} from './Export';
